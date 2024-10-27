d998918115a16e2a5dc91ddfa51d783dd61fdfe7
isky27 <gaganjindal50@gmail.com>
2024-10-27 16:23:31 +0530

    added address section

--- src/pages/account/ProfileWrapper.tsx
+++ src/pages/account/ProfileWrapper.tsx
@@ -10,13 +10,9 @@ const ProfileWrapper = ({ children }: any) => {
                     <div className="accountSidebar">
                         <h1>Account Dashboard</h1>
                         <ul className="sidebarNav">
-                            <li><Link to={"/"}>Account Dashboard</Link></li>
-                            <li><Link to={"/"}>Address Book</Link></li>
-                            <li><Link to={"/"}>Account Information</Link></li>
-                            <li><Link to={"/"}>my Order</Link></li>
-                            <li><Link to={"/"}>my Downloadable Product</Link></li>
-                            <li><Link to={"/"}>Newsletter Subscriptions</Link></li>
-                            <li><Link to={"/"}>Stored Payment Method</Link></li>
+                            <li><Link to={"/account"}>Account Dashboard</Link></li>
+                            <li><Link to={"/address"}>Address Book</Link></li>
+                            <li><Link to={"/"}>My Order</Link></li>
                             <li><Link to={"/"}>My Product Review</Link></li>
                         </ul>
                     </div>
